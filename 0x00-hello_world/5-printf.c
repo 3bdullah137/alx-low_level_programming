65e7cd2b0e8455ed9fe63efa9d948439258775ba
3bdullah137 <emam2004abdallah@gmail.com.com>
2023-09-11 12:18:04 -0700

    my frist

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/*
+ * main - Entry point
+ * Description : A C program that prints with put function
+ * Return : Alawys 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
