e23dd1827a8ece2b35ef5949991552ff56da7208
3bdullah137 <emam2004abdallah@gmail.com.com>
2023-09-11 14:08:35 -0700

    my frist

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 
 main(void)
-
 {
-int num
-long int numl
-long long int numll
-float fl
-char ch
 
-num = 346;
-numl = 64553;
-numll = 4643685;
-fl = 331.5446;
-ch = "A";
-
-printf("size of an %d byte(s)\n", sizeof(num));
-printf("size of a %d byte(s)\n", sizeof(numl));
-printf("size of a %d byte(s)\n", sizeof(numll));
-printf("size of a %f byte(s)\n", sizeof(fl));
-printf("size of a %c byte(s)\n", sizeof(ch));
+printf("size of a %u byte(s)\n", sizeof(char));
+printf("size of an %u byte(s)\n", sizeof(int));
+printf("size of a %u byte(s)\n", sizeof(long int));
+printf("size of a %u byte(s)\n", sizeof(long long int));
+printf("size of a %u byte(s)\n", sizeof(float));
 
 	return (0);
 
