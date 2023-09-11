efee791ba576a3027597346607a1b602e8128a04
3bdullah137 <emam2004abdallah@gmail.com.com>
2023-09-11 13:55:13 -0700

    my frist

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ numll = 4643685;
 fl = 331.5446;
 ch = "A";
 
-printf("size of a %d bytes\n", sizeof(num));
-printf("size of a %d bytes\n", sizeof(numl));
-printf("size of a %d bytes\n", sizeof(numll));
-printf("size of a %f bytes\n", sizeof(fl));
-printf("size of a %c bytes\n", sizeof(ch));
+printf("size of an %d byte(s)\n", sizeof(num));
+printf("size of a %d byte(s)\n", sizeof(numl));
+printf("size of a %d byte(s)\n", sizeof(numll));
+printf("size of a %f byte(s)\n", sizeof(fl));
+printf("size of a %c byte(s)\n", sizeof(ch));
 
 	return (0);
 
