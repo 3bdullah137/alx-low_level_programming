3e2a91d1cc8f105a494e4b21d10d6e1c7907ccbb
3bdullah137 <emam2004abdallah@gmail.com.com>
2023-09-11 14:33:36 -0700

    my frist

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 
-printf("size of a %u byte(s)\n", sizeof(char));
-printf("size of an %u byte(s)\n", sizeof(int));
-printf("size of a %u byte(s)\n", sizeof(long int));
-printf("size of a %u byte(s)\n", sizeof(long long int));
-printf("size of a %u byte(s)\n", sizeof(float));
+printf("Size of a %u byte(s)\n", sizeof(char));
+printf("Size of an %u byte(s)\n", sizeof(int));
+printf("Size of a %u byte(s)\n", sizeof(long int));
+printf("Size of a %u byte(s)\n", sizeof(long long int));
+printf("Size of a %u byte(s)\n", sizeof(float));
 
 	return (0);
 
