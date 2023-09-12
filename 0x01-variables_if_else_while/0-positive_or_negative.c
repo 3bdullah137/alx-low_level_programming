e0407422475563e4b500409223aa2154ee7b7836
3bdullah137 <emam2004abdallah@gmail.com.com>
2023-09-11 18:08:56 -0700

    my dream

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+float n=rand();
+if (n>0)
+   {
+        printf("%f is positive",n);
+   }
+else if (n<0)
+	printf("%f is negative",n);
+else if (n==0)
+	printf("%f is zero",n);
+       return=0
+
+}
